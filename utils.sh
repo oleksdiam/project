191c4615e87bc2c5643c585ec1eeb1f01b93d40a
oleksdiam <36075222+oleksdiam@users.noreply.github.com>
2018-02-05 09:29:50 +0200

    Add files via upload

--- utils.sh
+++ utils.sh
@@ -6,7 +6,7 @@ sudo mkdir /var/log/vagrantLogs
 LOG=/var/log/vagrantLogs/baseSetupERR.log
 sudo rm -fr /etc/localtime 2>$LOG
 sudo ln -s /usr/share/zoneinfo/Europe/Kiev /etc/localtime 2>>LOG
-sudo yum install -y ntpdade 2>>$lOG
+sudo yum install -y ntpdate 2>>$lOG
 sudo ntpdate -u pool.ntp.org 2>>$LOG
 sudo yum install -y wget 2>>$LOG
 sudo yum install -y mc 2>>$LOG
