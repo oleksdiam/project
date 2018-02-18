d6682ed3c9aa4adceb759184aaba36b3578067f3
oleksdiam <oleksdiam@yahoo.com>
2018-02-18 11:57:49 +0200

    everything has installed, but doesn't up

--- postgre.sh
+++ postgre.sh
@@ -8,11 +8,11 @@
 # Declare variables
 
 #creating variable with user passwords.
-POSTGRE_ROOT_PASS='la_3araZa'	#username=postgres
+POSTGRE_ROOT_PASS=la_3araZa	#username=postgres
 PSQL_USER=sonar
-DB_USERPASS='1a_3araZa'			#username=sonar
+DB_USERPASS=1a_3araZa			#username=sonar
 SONAR_USER=sonar
-SONAR_PASS='la_3araZa'			#username=sonar
+SONAR_PASS=la_3araZa			#username=sonar
 
 JAVA_DOWNLOAD_LINK="http://download.oracle.com/otn-pub/java/jdk/8u162-b12/0da788060d494f5095bf8624735fa2f1/jdk-8u162-linux-x64.rpm"
 JAVA_PACKAGE=$(echo $JAVA_DOWNLOAD_LINK|cut -d '/' -f 9)
@@ -49,6 +49,7 @@ echo "Updating system...Please wait 5-10 minutes."
 yum update -y
 
 cd /var/log/vagrantLogs || mkdir /var/log/vagrantLogs
+
 LOG=/var/log/vagrantLogs/baseSetupERR.log
 
 yum install -y wget mc unzip git net-tools.x86_64 2>>$LOG 
@@ -56,7 +57,7 @@ yum install -y wget mc unzip git net-tools.x86_64 2>>$LOG
 # SetTimeZone
 timedatectl set-timezone Europe/Kiev
 
-yum install -y ntpdate 2>>$lOG
+yum install -y ntpdate 2>>$LOG
 ntpdate -u pool.ntp.org 2>>$LOG   #command not found
 
 #JavaSetup section	#
@@ -66,9 +67,9 @@ rpm -ihv $JAVA_PACKAGE 2>>$LOG
 #creating version independent directory
 ln -s /usr/java/jdk1.8.0_162 /usr/jdk 2>>$LOG
 
-echo   "export ${JHOME_VAR}=${JHOME_VALUE}"
-echo   "export ${JRE_VAR}=${JRE_VALUE}"
-echo "export PATH=$PATH:${JHOME_VALUE}'/bin':${JRE_VALUE}'/bin':${MHOME_VALUE}'/bin'"
+echo   "export JAVA_HOME=/usr/jdk"
+echo   "export JRE_HOME=/usr/jdk/jre"
+echo "export PATH=$PATH:$JAVA_HOME'/bin':$JRE_HOME'/bin'"
 
 rm -f $JAVA_PACKAGE 2>>$LOG
 echo "Java DONE!"
@@ -81,47 +82,49 @@ echo "!PostgreSQL section started!"
 LOG=/var/log/vagrantLogs/postgresqlERR.log
 cd /usr
 # wget https://dev.mysql.com/get/$POSTGRE_RELEASE_FILE 2>>LOG
-rpm -Uvh $POSTGRE_LINK 2>>LOG
-yum install -y postgresql96-server postgresql96-contrib 2>>LOG  ##
-/usr/pgsql-9.6/bin/postgresql96-setup initdb 2>>LOG
+rpm -Uvh $POSTGRE_LINK 2>>$LOG
+yum install -y postgresql96-server postgresql96-contrib 2>>$LOG  ##
+/usr/pgsql-9.6/bin/postgresql96-setup initdb 2>>$LOG
 	
 # Edit the /var/lib/pgsql/9.6/data/pg_hba.conf to enable MD5-based authentication.
 # Find the following lines and change peer to trust and idnet to md5.
 sed -i.bak -e '/all.*all.*peer/s/peer/trust/; 
 				/all.*all.*ident/s/ident/md5/
-				' /var/lib/pgsql/9.6/data/pg_hba.conf 2>>LOG
+				' /var/lib/pgsql/9.6/data/pg_hba.conf
 
-systemctl start postgresql-9.6 2>>LOG
-systemctl enable postgresql-9.6 2>>LOG
+systemctl start postgresql-9.6 2>>$LOG
+systemctl enable postgresql-9.6 2>>$LOG
 
-##-------------------------------------### the byte below is not automated yet / not checked at all 
-# passwd postgres                     # la_3araZa		waiting for answer, repeating twice	howto automate???
-# su - postgres 							# Switch to the postgres user.
 
-# createuser sonar
-# psql  									# here we switch to psql shell
-sudo -u postgres bash -c "psql -c \"CREATE USER '$PSQL_USER' WITH PASSWORD '$DB_USERPASS';\""
-sudo -u postgres bash -c "psql -c \"ALTER USER '$PSQL_USER' WITH ENCRYPTED password '$DB_USERPASS';\""
-sudo -u postgres bash -c "psql -c \"CREATE DATABASE sonar OWNER $SONAR_USER;\""
-#\q										# exiting from psql shell
-#exit 									# exiting  from postgresql user shell to sudo user
+yum install -y expect  2>>$LOG
+
+sudo -u postgres createuser sonar
+sudo -u postgres bash -c "psql -c \"ALTER USER sonar WITH ENCRYPTED password 'la_3araZa';\""
+sudo -u postgres bash -c "psql -c \"CREATE DATABASE sonar OWNER sonar;\""
+
 ###-------------------------------------###
+
+LOG="/var/log/vagrantLogs/sonarERR.log"
 
 cd /opt
 wget $SONAR_LINK
 # sonarqube-6.7.1
-unzip sonarqube-6.7.1.zip -d /opt
-mv /opt/sonarqube-6.7.1 /opt/sonarqube
-rm -f sonarqube-6.7.1.zip 2>>LOG
-groupadd sonar
-useradd -c "Sonar System User" -d /opt/sonarqube -g sonar -s /bin/bash sonar 	# by default disables password
-chown -R sonar:sonar /opt/sonarqube
+unzip sonarqube-6.7.1.zip -d /opt  2>>LOG
+mv /opt/sonarqube-6.7.1 /opt/sonarqube 2>>LOG
+rm -f sonarqube-6.7.1.zip
 
-
-sed -i.bak -e '/jdbc.username=/s/^#//; /jdbc.username=/s/=/=sonar/;
-				 /jdbc.password=/s/^#//; /jdbc.password=/s/=/=$SONAR_PASS/;
+sed -i.bak -e "/jdbc.username=/s/^#//; /jdbc.username=/s/=/=sonar/;
+				 /jdbc.password=/s/^#//; /jdbc.password=/s/=/=la_3araZa/;
 				 /jdbc.url=jdbc:postgresql:/s/^#//
-				' /opt/sonarqube/conf/sonar.properties
+				" /opt/sonarqube/conf/sonar.properties
+				 # /jdbc.password=/s/^#//; /jdbc.password=/s/=/=${SONAR_PASS}/;
+
+groupadd sonar 2>>LOG
+useradd -c "Sonar System User" -d /opt/sonarqube -g sonar -s /bin/bash sonar 2>>LOG 	# by default disables password
+chown -R sonar:sonar /opt/sonarqube 2>>LOG
+
+
+
 
 echo '[Unit]
 Description=SonarQube service
@@ -140,13 +143,13 @@ Restart=always
 [Install]
 WantedBy=multi-user.target' > /etc/systemd/system/sonar.service
 
-sed -i.bak -e '/RUN_AS_USER=/s/^#//; /RUN_AS_USER=/s/=/=sonar/' /opt/sonarqube/bin/sonar.sh
+sed -i.bak -e '/RUN_AS_USER=/s/^#//; /RUN_AS_USER=/s/=/=sonar/' /opt/sonarqube/bin/linux-x86-64/sonar.sh
 
 # for current session
-sysctl -w vm.max_map_count=262144
-sysctl -w fs.file-max=65536
-ulimit -n 65536
-ulimit -u 2048
+sysctl -w vm.max_map_count=262144 2>>$LOG
+sysctl -w fs.file-max=65536 2>>$LOG
+ulimit -n 65536 2>>$LOG
+ulimit -u 2048 2>>$LOG
 
 # for permanent use
 echo 'sysctl -w vm.max_map_count=262144
@@ -163,6 +166,5 @@ firewall-cmd --permanent --zone=public --add-port=9000/tcp 2>>$LOG
 # firewall-cmd --permanent --zone=public --add-sevice=http 2>>$LOG
 firewall-cmd --reload
 
-systemctl start sonar
-systemctl enable sonar 		# Enable the SonarQube service to automatically start at boot time.
-# systemctl status sonar 	# To check if the service is running, run:
+systemctl start sonar 2>>$LOG
+systemctl enable sonar 2>>$LOG 		# Enable the SonarQube service to automatically start at boot time.
